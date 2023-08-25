7a36029264a00af9dc8ba35eb773c476fde96244
saraivajv <joaovictors2010@gmail.com>
2023-08-25 17:15:21 -0300

    Bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './components/Home'
+import Main from './components/Main'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Main />
     </div>
   );
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import '../App';
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card">
+            <img src="%PUBLIC_URL%/logo192.png" class="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }
